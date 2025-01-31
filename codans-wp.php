0fd14715f20549f92631a50b4379c1dfa55e7e21
juniorbotelho <junowerther@gmail.com>
2025-01-30 23:04:13 -0300

    feat: add log email utility

---
+++ codans-wp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Codans WP
+ * Description: Official codans plugin that gives Wordpress superpowers to handle the company's own relationships.
+ * Plugin URI:  https://github.com/juniorbotelho/codans-wp
+ * Version:     1.0.0
+ * Author:      Junior Botelho
+ * Author URI:  https://github.com/juniorbotelho/
+ * Text Domain: codans-wp
+ *
+ * Requires Plugins: elementor
+ * Elementor tested up to: 3.25.0
+ * Elementor Pro tested up to: 3.25.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
