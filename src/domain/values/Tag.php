d3d776223492fb15f64cfbae97ff3ba7820c2a2c
juniorbotelho <junowerther@gmail.com>
2025-02-04 19:59:43 -0300

    feat: add tag value-object

---
+++ src/domain/values/Tag.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Codans\Domain\Interfaces\Values\ITag;
+
+class Tag implements ITag
+{
+	/**
+	 * Tag value object.
+	 *
+	 * @param string $name
+	 * @param int $tag
+	 */
+	public function __construct(
+		private readonly string $name,
+		private readonly int $tag,
+	) {
+	}
+}
